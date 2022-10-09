4f137d3bf8b35733b269d9b02beb78b4975c23b7
Sanskar Malkhede <85408959+sanskarmalkhede@users.noreply.github.com>
2022-10-09 15:58:36 +0530

    Add files via upload

---
+++ C#/even-or-odd.cs
@@ -0,0 +1,11 @@
+int x = 125;
+
+switch (x % 2)
+{ 
+    case 0:
+        Console.WriteLine($"{x} is an even value");
+        break;
+    case 1:
+        Console.WriteLine($"{x} is an odd Value");
+        break;
+}
