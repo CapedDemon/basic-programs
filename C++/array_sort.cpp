06c431af0128cedc2d32704bed108bf230517b38
Panav <63401208+Pana1v@users.noreply.github.com>
2022-10-14 17:20:31 +0530

    array_sort.cpp
    This program inputs size and sorts arrays in C++

---
+++ C++/array_sort.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+        int tmp;
+         for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(a[i]>a[j])
+                {
+                    tmp=a[i];
+                    a[i]=a[j];
+                    a[j]=tmp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+    }
+    
+
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
